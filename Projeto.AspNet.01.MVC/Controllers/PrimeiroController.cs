d7aabe8386cfe4bca25d5e6b121265b63107d3ce
BrunoMano2004 <brunomano2004@gmail.com>
2023-08-29 12:51:30 -0300

    CSharp.AspNet.01

---
+++ Projeto.AspNet.01.MVC/Controllers/PrimeiroController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto.AspNet._01.MVC.Models;
+
+namespace Projeto.AspNet._01.MVC.Controllers
+{
+    public class PrimeiroController : Controller
+    {
+        /*
+        public string Index()
+        {
+            return "Olá Mundo Asp.Net";
+        }
+
+        public IActionResult Ola()
+        {
+            ViewBag.Message = "Tô começando a sacar como a coisa funciona!";
+            return View();
+        }
+        */
+
+        public IActionResult Credenciais()
+        {
+            Perfil infos = new Perfil();
+            infos.Nome = "Zé Pequeno";
+            infos.Idade = 22;
+            infos.Endereco = "Cidade de Deus";
+            return View(infos);
+        }
+    }
+}
---
+++ Projeto.AspNet.01.MVC/Models/Perfil.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto.AspNet._01.MVC.Models
+{
+    public class Perfil
+    {
+        public string? Nome {get; set;}
+        public int? Idade { get; set;}
+        public string? Endereco { get; set;}
+    }
+}
